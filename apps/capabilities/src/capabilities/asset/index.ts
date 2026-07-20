3720a8bb2bec83e56a5f5722703a237317662849
DHRUVILPATEL2303 <pateldhruvil2303@gmail.com>
2026-07-20 19:33:37 +0530

    feat(capabilities): move asset operation under stellar capability (GET /stellar/asset)

--- apps/capabilities/src/capabilities/asset/index.ts
+++
@@ -1,6 +0,0 @@
-import type { CapabilityModule } from "../../types";
-import { routes } from "./routes";
-import { manifest } from "./manifest";
-
-/** The Stellar Asset capability: supply, holders, and flags for an asset. */
-export const capability: CapabilityModule = { routes, manifest };
